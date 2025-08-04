91eac4f598f32e5c0ec69b32918cd68da08d739c
Jacob Sandlund <jacob@jacobsandlund.com>
2025-08-04 09:26:37 -0400

    no tests

--- build.zig
+++ build.zig
@@ -10,23 +10,9 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const lib_mod = b.addModule("uucode.x", .{
+    _ = b.addModule("uucode.x", .{
         .root_source_file = b.path("src/x_root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
 }
