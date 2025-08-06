7c61e91b1e1b3cd5772128fc970db73fca42da32
Jacob Sandlund <jacob@jacobsandlund.com>
2025-08-06 09:19:55 -0400

    config.x.zig

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("uucode.x.config", .{
+    _ = b.addModule("config.x.zig", .{
         .root_source_file = b.path("src/config/root.zig"),
         .target = target,
         .optimize = optimize,
@@ -18,9 +18,9 @@ pub fn build(b: *std.Build) void {
 }
 
 pub fn connectBuild(uucode_x: *std.Build.Dependency, uucode: *std.Build.Dependency) void {
-    const config_mod = uucode_x.module("uucode.x.config");
+    const config_mod = uucode_x.module("config.x.zig");
 
-    uucode.module("build_config").addImport("uucode.x.config", config_mod);
+    uucode.module("build_config").addImport("config.x.zig", config_mod);
     config_mod.addImport("config.zig", uucode.module("config.zig"));
     config_mod.addImport("types.zig", uucode.module("types.zig"));
 }
